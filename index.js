7b38ff0c484b5dd55397908f66becb80282f46a8
Benjamin Monk <Bmonk1996@yahoo.com>
2023-02-03 08:55:30 -0600

    finished with game, everything is working

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-let score = 4;
+let score = 0;
 
 
 const holes = document.getElementsByClassName("hole");
-console.log(holes)
+const htmlScore = document.getElementById('score');
+
+
+function createMole(){
+    const randomHoleIndex = Math.floor(Math.random() * holes.length);
+    holes[randomHoleIndex].classList.toggle("mole");
+    
+}
+
+function whackMole(clickEvent){
+    if(clickEvent.target.matches('.mole')){
+        score++
+        clickEvent.target.classList.remove('mole')
+    }else{
+        score--
+    }
+    htmlScore.innerHTML = score
+}
+
+
+setInterval(createMole, 500)
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', whackMole)
